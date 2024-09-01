76948af2415bfda9fbcbd385ff36928938d979ee
mahmudul-hasan <mhmahmudul1211@gmail.com>
2024-09-01 23:01:29 +0600

    first commit

---
+++ multiple condition.js
@@ -0,0 +1,10 @@
+var salary = 32000;
+var height = 66;
+var carOwner = false;
+
+if ((salary > 2500 || height > 60) && carOwner == true){
+    console.log('Accepted')
+}
+else{
+    console.log('not accepted')
+}
